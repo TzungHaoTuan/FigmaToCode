b22a64c4d9e46b80ed4163f001a5b93e0c1fac56
Elliott Tuan <gbs.tuan@gmail.com>
2023-07-08 09:17:05 +0800

    Fix vercel error messages of module not found

--- figma-to-code/next.config.js
+++ figma-to-code/next.config.js
@@ -1,9 +1,4 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  eslint: {
-    // Enable ESLint during production builds
-    ignoreDuringBuilds: false,
-  },
-};
+const nextConfig = {};
 
 module.exports = nextConfig;

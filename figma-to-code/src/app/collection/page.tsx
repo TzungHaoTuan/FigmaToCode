1c6d3f72317e8dca5f83a32b9193e07803d4d4c3
Elliott Tuan <gbs.tuan@gmail.com>
2023-07-15 17:58:42 +0800

    Add syntax highlight to code in collection page

--- figma-to-code/src/app/collection/page.tsx
+++ figma-to-code/src/app/collection/page.tsx
@@ -1,17 +1,12 @@
 "use client";
 import { Provider } from "react-redux";
 import store from "@/store/store";
-
-import React from "react";
-import Link from "next/link";
-
 import Grid from "./component/grid";
 
 export default function Collection() {
   return (
     <Provider store={store}>
       <Grid />
-      {/* <div>{data && data.document.children}</div> */}
     </Provider>
   );
 }
